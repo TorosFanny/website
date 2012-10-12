60ced1ce8843211b0606905ac49edef29b70e7e5
Francesco <f@mazzo.li>
2012-10-12 19:35:05 +0100

    cv

---
+++ Makefile
@@ -0,0 +1,18 @@
+HTML_FILES = logo.png
+
+all: cv.html cv.pdf cv.txt
+
+cv.html: source.html $(HTML_FILES)
+	sed 's/%%formats%%/<li class="formats"><a href=".\/cv.txt">Plain text<\/a> \&middot; <a href=".\/cv.pdf">Pdf<\/a><\/li>/' source.html > $@
+
+cv-nofmts.html: source.html $(HTML_FILES)
+	sed 's/%%formats%%//' source.html > $@
+
+cv.txt: cv-nofmts.html plaintext.hs
+	runhaskell plaintext.hs < cv-nofmts.html > $@
+
+cv.pdf: cv-nofmts.html
+	./makepdf $(HTML_FILES)
+
+clean:
+	rm -f cv.html cv-nofmts.html cv.txt cv.pdf
---
+++ plaintext.hs
@@ -0,0 +1,25 @@
+import Text.Pandoc
+
+autoLink :: Inline -> Inline
+autoLink (Link ils tg) = Link [Code nullAttr (concat [s | Str s <- ils])] tg
+autoLink il            = il
+
+strongToEmph :: Inline -> Inline
+strongToEmph (Strong ils) = Emph ils
+strongToEmph il           = il
+
+stripImages :: Inline -> [Inline]
+stripImages (Image ils _) = ils
+stripImages il            = [il]
+
+transformDoc :: Pandoc -> Pandoc
+transformDoc = bottomUp (concatMap stripImages) . bottomUp (strongToEmph . autoLink)
+
+readDoc :: String -> Pandoc
+readDoc = readHtml defaultParserState
+
+writeDoc :: Pandoc -> String
+writeDoc = writeMarkdown defaultWriterOptions
+
+main :: IO ()
+main = interact (writeDoc . transformDoc . readDoc)
