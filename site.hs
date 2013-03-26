cb05cc0a3053d0cc167389acf179f5031731fa31
Francesco Mazzoli <f@mazzo.li>
2013-03-26 21:49:09 +0100

    rss/atom feed

--- site.hs
+++ site.hs
@@ -5,34 +5,48 @@ import           Hakyll
 main :: IO ()
 main =
     hakyll $
-    do match "images/*" $ do route idRoute; compile copyFileCompiler
+    do -- Assets
+       match "images/*" $ do route idRoute; compile copyFileCompiler
        match "css/*"    $ do route idRoute; compile copyFileCompiler
        match "js/*"     $ do route idRoute; compile copyFileCompiler
        match "fonts/*"  $ do route idRoute; compile copyFileCompiler
 
+       -- Templates
+       match "templates/*" $ compile templateCompiler
+
+       -- Posts
        match "posts/*" $
            do route $ setExtension "html"
               compile $ pandocCompiler                                        >>=
                         loadAndApplyTemplate "templates/post.html"    postCtx >>=
+                        saveSnapshot "content"                                >>=
                         loadAndApplyTemplate "templates/default.html" postCtx >>=
                         relativizeUrls
 
+       -- Archive
        create ["archive.html"] $
            do route idRoute
               compile $
                   let archiveCtx = field "posts" (\_ -> postList recentFirst) <>
                                    constField "title" "index"                 <>
                                    defaultContext
-                  in makeItem ""                                              >>=
-                     loadAndApplyTemplate "templates/archive.html" archiveCtx >>=
-                     loadAndApplyTemplate "templates/default.html" archiveCtx >>=
-                     relativizeUrls
+                  in  makeItem ""                                              >>=
+                      loadAndApplyTemplate "templates/archive.html" archiveCtx >>=
+                      loadAndApplyTemplate "templates/default.html" archiveCtx >>=
+                      relativizeUrls
 
-       match "templates/*" $ compile templateCompiler
-
+       -- Splash page
        match "index.html" $
-           do route idRoute
-              compile $ getResourceBody >>= relativizeUrls
+           do route idRoute; compile $ getResourceBody >>= relativizeUrls
+
+       -- RSS and Atom
+       create ["atom.xml"] $ renderFeed renderAtom
+       create ["rss.xml"]  $ renderFeed renderRss
+       -- -- CV
+       -- match "cv.htm" $
+       --     do route idRoute
+       --        compile $ getResourceBody >>= relativiseUrls
+
 
 postCtx :: Context String
 postCtx = dateField "date" "%B %e, %Y" <> defaultContext
@@ -43,3 +57,22 @@ postList sortFilter =
        itemTpl <- loadBody "templates/post-item.html"
        list    <- applyTemplateList itemTpl postCtx posts
        return list
+
+renderFeed
+    :: (FeedConfiguration -> Context String -> [Item String] -> Compiler (Item String))
+    -> Rules ()
+renderFeed f =
+    do route idRoute
+       compile $ do let feedCtx = postCtx <> bodyField "description"
+                    posts <- fmap (take 10) . recentFirst =<<
+                             loadAllSnapshots "posts/*" "content"
+                    f feedConf feedCtx posts
+
+feedConf :: FeedConfiguration
+feedConf = FeedConfiguration
+    { feedTitle       = "bitonic's blog."
+    , feedDescription = "Computers, with a PL theory slant."
+    , feedAuthorName  = "Francesco Mazzoli"
+    , feedAuthorEmail = "f@mazzo.li"
+    , feedRoot        = "http://mazzo.li"
+    }
